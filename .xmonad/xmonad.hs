7de5adf62c341d6e0af373b6099b954764e444df
Michael Thelander <mjthelander@gmail.com>
2013-03-10 18:46:00 -0700

    Commit all dot files.

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,17 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+xmproc <- spawnPipe "/usr/bin/xmobar /home/mjthelander/.xmobarrc"
+xmonad $ defaultConfig
+    { modMask = mod4Mask -- Use Super instead of Alt
+    , terminal = "gnome-terminal"
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    } `additionalKeys`
+    [ 
+    ]
