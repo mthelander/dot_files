e16bc976ec9c6ac2ebefe2defbec7e3edbc4b68c
Michael Thelander <michael.thelander@rentrakmail.com>
2013-07-17 13:04:44 -0700

    Add xmonad keybindings
    - Launch dmenu with .scripts/dmenu-solarized.
    - Always float Empathy.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,14 +4,21 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Graphics.X11.ExtraTypes.XF86
+
+noTileHook = composeAll
+    [ className =? "Empathy" --> doFloat
+    ]
 
 main = do
-xmproc <- spawnPipe "/usr/bin/xmobar /home/mjthelander/.xmobarrc"
-xmonad $ defaultConfig
-    { modMask = mod4Mask -- Use Super instead of Alt
-    , terminal = "terminator"
-    , manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    } `additionalKeys`
-    [ 
-    ]
+  xmproc <- spawnPipe "/usr/bin/xmobar /home/mthelander/.xmobarrc"
+  xmonad $ defaultConfig
+      { modMask = mod4Mask -- Use Super instead of Alt
+      , terminal = "terminator"
+      , manageHook = manageDocks <+> noTileHook <+> manageHook defaultConfig
+      , layoutHook = avoidStruts $ layoutHook defaultConfig
+      } `additionalKeys`
+      [ ((0,        xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
+      , ((0,        xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
+      , ((mod4Mask, xK_p                   ), spawn "$HOME/.scripts/dmenu-solarized")
+      ]
