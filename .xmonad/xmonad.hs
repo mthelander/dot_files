6fdeafcf1f2d08a6d7c749c21bd9f8b2c39a916d
Michael Thelander <michael.thelander@rentrakmail.com>
2013-09-20 10:46:10 -0700

    Configure xmonad.hs to play nice with Xfce
    Note: this deprecates the use of xmobar in favor of Xfce's panels.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,20 +5,24 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import Graphics.X11.ExtraTypes.XF86
+import XMonad.Hooks.EwmhDesktops
 
 noTileHook = composeAll
     [ className =? "Empathy" --> doFloat
     ]
 
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar /home/mthelander/.xmobarrc"
   xmonad $ defaultConfig
-      { modMask = mod4Mask -- Use Super instead of Alt
-      , terminal = "terminator"
-      , manageHook = manageDocks <+> noTileHook <+> manageHook defaultConfig
-      , layoutHook = avoidStruts $ layoutHook defaultConfig
+      { modMask         = mod4Mask -- Use Super instead of Alt
+      , terminal        = "terminator"
+      , manageHook      = manageDocks <+> noTileHook <+> manageHook defaultConfig
+      , logHook         = ewmhDesktopsLogHook
+      , handleEventHook = ewmhDesktopsEventHook
+      , startupHook     = ewmhDesktopsStartup
+      , layoutHook      = avoidStruts $ layoutHook defaultConfig
       } `additionalKeys`
       [ ((0,        xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
       , ((0,        xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
       , ((mod4Mask, xK_p                   ), spawn "$HOME/.scripts/dmenu-solarized")
+      , ((mod4Mask .|. shiftMask, xK_q     ), spawn "xfce4-session-logout")
       ]
