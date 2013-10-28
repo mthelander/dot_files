92689dcb836c9192e7588f48f418251fc7efa1ca
Michael Thelander <michael.thelander@rentrakmail.com>
2013-10-28 14:09:40 -0700

    Remove shortcut to spawn dmenu, since xfce is overriding it anyway.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -24,6 +24,7 @@ main = do
       } `additionalKeys`
       [ ((0,        xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
       , ((0,        xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
-      , ((mod4Mask, xK_p                   ), spawn "$HOME/.scripts/dmenu-solarized")
+      --, ((mod4Mask, xK_p                   ), spawn "dmenu_run -b -nb 'black' -nf '#17c200' -sb '#079dad'")
       , ((mod4Mask .|. shiftMask, xK_q     ), spawn "xfce4-session-logout")
+      , ((mod4Mask, xK_o                   ), spawn "google-chrome")
       ]
