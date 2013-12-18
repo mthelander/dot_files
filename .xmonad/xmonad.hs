6c99e8a8c8e720c6c292437d0e34f535348fe5dc
Michael Thelander <michael.thelander@rentrakmail.com>
2013-12-18 09:39:57 -0800

    Don't let libnotify steal focus in xmonad
    Also, use the default xfce settings.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Config.Xfce
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -8,23 +9,23 @@ import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.EwmhDesktops
 
 noTileHook = composeAll
-    [ className =? "Empathy" --> doFloat
+    [ className =? "Xfce4-notifyd" --> doIgnore
     ]
 
 main = do
-  xmonad $ defaultConfig
+  xmonad $ xfceConfig
       { modMask         = mod4Mask -- Use Super instead of Alt
       , terminal        = "terminator"
-      , manageHook      = manageDocks <+> noTileHook <+> manageHook defaultConfig
+      , manageHook      = manageDocks <+> noTileHook <+> manageHook xfceConfig
       , logHook         = ewmhDesktopsLogHook
       , handleEventHook = ewmhDesktopsEventHook
       , startupHook     = ewmhDesktopsStartup
       , focusedBorderColor = "#268bd2"
-      , layoutHook      = avoidStruts $ layoutHook defaultConfig
+      , layoutHook      = avoidStruts $ layoutHook xfceConfig
       } `additionalKeys`
       [ ((0,        xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
       , ((0,        xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
-      --, ((mod4Mask, xK_p                   ), spawn "dmenu_run -b -nb 'black' -nf '#17c200' -sb '#079dad'")
+      , ((mod4Mask, xK_m                   ), spawn "dmenu_run -b -nb 'black' -nf '#17c200' -sb '#079dad'")
       , ((mod4Mask .|. shiftMask, xK_q     ), spawn "xfce4-session-logout")
       , ((mod4Mask, xK_o                   ), spawn "google-chrome")
       , ((mod4Mask, xK_u                   ), spawn "setxkbmap -option ctrl:nocaps")
